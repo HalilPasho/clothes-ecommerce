caeb4134758abff9b3e92ef422290209eba231b8
Halil <halilpasho1@gmail.com>
2020-05-31 23:03:14 +0200

    created components for homepage

--- clothes/src/App.js
+++ clothes/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import Homepage from './pages/homepage/homepage'
 
 function App() {
-  return <div className="App">Test</div>;
+  return <div className="App"><Homepage /></div>;
 }
 
 export default App;
